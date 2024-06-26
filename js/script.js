6ada7a9008be52538f964549b095a8f532b90b42
ceccobell <francescobell2001@gmail.com>
2024-06-26 15:50:43 +0200

    [UPD] added all function for ticket

--- js/script.js
+++ js/script.js
@@ -10,9 +10,19 @@ generate_btn.addEventListener('click', function() {
     if (age == "minorenne") {
         ticket_price -= ticket_price * 0.2;
         console.log(ticket_price)
+        // piccola funzione che inserisce il tipo di biglietto a schermo in base all'età 
+        document.getElementById("ticket_type").innerText = "Biglietto under 18"
     } else if (age == "senior") {
         ticket_price -= ticket_price * 0.4;
         console.log(ticket_price)
+        // piccola funzione che inserisce il tipo di biglietto a schermo in base all'età
+        document.getElementById("ticket_type").innerText = "Biglietto over 65"
+    } else if (age == "adulto") {
+        // piccola funzione che inserisce il tipo di biglietto a schermo in base all'età
+        document.getElementById("ticket_type").innerText = "Biglietto Standard"
     }
-    console.log(ticket_price)
+    ticket_price = ticket_price.toFixed(2);
+    document.getElementById("name").innerText = full_name
+    document.getElementById("ticket_price").innerText = ticket_price 
 })
+
