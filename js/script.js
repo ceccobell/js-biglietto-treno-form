6767f24261d242fa4dfa57b6a0d1516368b9a48e
ceccobell <francescobell2001@gmail.com>
2024-06-26 15:20:29 +0200

    [UPD] now function take values and calculate ticket price

--- js/script.js
+++ js/script.js
@@ -2,8 +2,17 @@
 const generate_btn = document.getElementById("calculate-price");
 generate_btn.addEventListener('click', function() {
     let full_name = document.getElementById("full_name").value;
-    console.log(full_name);
-
     let km = document.getElementById("km").value; 
-    console.log(km);
+    let age = document.getElementById("select-age").value;
+
+    let ticket_price = km * 0.21
+
+    if (age == "minorenne") {
+        ticket_price -= ticket_price * 0.2;
+        console.log(ticket_price)
+    } else if (age == "senior") {
+        ticket_price -= ticket_price * 0.4;
+        console.log(ticket_price)
+    }
+    console.log(ticket_price)
 })
