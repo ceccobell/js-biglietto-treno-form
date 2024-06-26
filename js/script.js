1bf5c8f8e06d8de2e6b1f43b3c206ff4cccd8667
ceccobell <francescobell2001@gmail.com>
2024-06-26 15:10:25 +0200

    [UPD] created function in js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-let full_name = document.getElementById("full_name").value;
-console.log(full_name);
 
-let km = document.getElementById("km").value; 
-console.log(km);
+const generate_btn = document.getElementById("calculate-price");
+generate_btn.addEventListener('click', function() {
+    let full_name = document.getElementById("full_name").value;
+    console.log(full_name);
+
+    let km = document.getElementById("km").value; 
+    console.log(km);
+})
